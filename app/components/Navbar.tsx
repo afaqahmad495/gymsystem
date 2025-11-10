01d7b25dd2136cc56ecc1475d8ab5e6fd9841868
afaqahmad495 <afaqahmad62517@gmail.com>
2025-11-10 10:55:54 +0500

    code updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -36,12 +36,12 @@ const Navbar: React.FC = () => {
             </Link>
           </div>
 
-          <div className="md:hidden flex items-center justify-center">
+          <div className="md:hidden flex items-center justify-center ">
             <button
               onClick={() => setOpen((s) => !s)}
               aria-expanded={open}
               aria-label="Toggle menu"
-              className=" rounded-md text-gray-700  transition-all"
+              className=" rounded-md text-gray-700  "
             >
               {!open ? (
                 <div className="flex items-center justify-center pb-5 ml-2"> 
@@ -49,7 +49,7 @@ const Navbar: React.FC = () => {
                 </div>
                 
               ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2 " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 </svg>
               )}
@@ -59,7 +59,7 @@ const Navbar: React.FC = () => {
       </div>
 
       {open && (
-        <div className="md:hidden px-2 pb-4 space-y-1 transition-all duration-300 ease-in-out">
+        <div className="md:hidden px-2 pb-4 space-y-1 ">
           <Link href="/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:bg-red-100 dark:text-gray-800">
             Home
           </Link>
